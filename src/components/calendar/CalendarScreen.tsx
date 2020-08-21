4e2c64eb9ed1913a342f9cf4059af13364e9aa93
Jose Banega <banegajose26@gmail.com>
2020-08-21 00:42:41 -0300

    Add big calendar & moment

--- src/components/calendar/CalendarScreen.tsx
+++ src/components/calendar/CalendarScreen.tsx
@@ -1,10 +1,23 @@
+import moment from 'moment';
 import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
 import Header from '../ui/Header';
+
+const localizer = momentLocalizer(moment);
+const events = [
+  {
+    title: 'Boss birthday',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa'
+  }
+];
 
 const CalendarScreen = () => {
   return (
     <React.Fragment>
       <Header></Header>
+      <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end" />
     </React.Fragment>
   );
 };
