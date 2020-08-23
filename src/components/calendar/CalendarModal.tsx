a13a526187bbee9e7812e789994d4c7d8230cb41
Jose Banega <banegajose26@gmail.com>
2020-08-22 22:04:39 -0300

    Styling Calendar Modal Component

--- src/components/calendar/CalendarModal.tsx
+++ src/components/calendar/CalendarModal.tsx
@@ -11,18 +11,74 @@ const customStyles = {
     transform: 'translate(-50%, -50%)'
   }
 };
-
+Modal.setAppElement('#root');
 const CalendarModal = () => {
-  const modalIsOpen = false;
+  const modalIsOpen = true;
+
+  const onRequestClose = () => {
+    console.log('clossing modall');
+  };
   return (
     <div>
       <Modal
         isOpen={modalIsOpen}
         // onAfterOpen={afterOpenModal}
-        // onRequestClose={closeModal}
+        onRequestClose={onRequestClose}
+        closeTimeoutMS={200}
         style={customStyles}
-        contentLabel="Example Modal">
-        <p>Calendar Modal</p>
+        contentLabel="Modal">
+        <h1 className="block w-full text-center text-xl text-gray-800 font-bold pb-1"> Nuevo evento </h1>
+        <hr className="mb-3" />
+        <form className="container">
+          <div className="mb-4 mt-1">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Fecha y hora inicio</label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Fecha inicio"
+            />
+          </div>
+
+          <div className="mb-4 mt-1">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Fecha y hora fin</label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Fecha inicio"
+            />
+          </div>
+
+          <hr />
+          <div className="mb-4 mt-1">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Titulo y notas</label>
+            <input
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Título del evento"
+              name="title"
+              autoComplete="off"
+            />
+            <small id="emailHelp" className="text-gray-600 text-xs italic">
+              Una descripción corta
+            </small>
+          </div>
+
+          <div className="mb-4 mt-1">
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Notas"
+              rows={5}
+              name="notes"></textarea>
+            <small id="emailHelp" className="text-gray-600 text-xs italic">
+              Información adicional
+            </small>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded">
+            <i className="far fa-save"></i>
+            <span> Guardar</span>
+          </button>
+        </form>
       </Modal>
     </div>
   );
